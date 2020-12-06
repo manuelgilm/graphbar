f1e3138df2a25f3d85a0b7c2b9d6471266d92ef7
Manuel Gil <manuelgilsitio@gmail.com>
2020-12-06 03:03:18 -0500

    first commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="graphbar",
+    version="0.0.1",
+    author="Manuel Gil",
+    author_email="manuelgilsitio@gmail.com",
+    description="Make bar graph by groups",
+    url="https://github.com/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
